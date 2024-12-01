268fb6f29237d5dd206d504c2a07f2feb4b74c3c
FRcenten <fr.centeno@duocuc.cl>
2024-12-01 18:59:17 -0300

    nuevo

--- Fase 2/Evidencias Proyecto/Evidencias del Sistema/src/environments/environment.ts
+++ Fase 2/Evidencias Proyecto/Evidencias del Sistema/src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBGElM6-S6n4dV5Uw4CA9ifVCwYLUKZF08",
-    authDomain: "qrcheck-nnspa.firebaseapp.com",
-    projectId: "qrcheck-nnspa",
-    storageBucket: "qrcheck-nnspa.appspot.com",
-    messagingSenderId: "1079216645952",
-    appId: "1:1079216645952:web:fd6c59cc1770cbe1033ca5",
-    measurementId: "G-GG3LGCNC1G"
-  }
+  production: false
 };
 
 /*
